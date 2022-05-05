3bcff8772c79266ad28b2a2d5e78a7027b743da5
Sai Buman <78092854+SaiBuman@users.noreply.github.com>
2022-05-05 23:41:35 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,11 +1,12 @@
-song1 = "";
-song2 = "";
+song = "";
+left_Wrist_x = "" ;
+right_Wrist_x = "" ;
 
+left_Wrist_y = "" ;
+right_Wrist_y = "" ;
 
 function preload() {
-    song1 = loadSound("Savage Love.mp3");
-    song2 = loadSound("Jugnu.mp3");
-
+    song = loadSound("music.mp3")
 }
 
 function setup() {
@@ -14,8 +15,41 @@ function setup() {
 
      video = createCapture(VIDEO);
      video.hide();
+
+     poseNet = ml5.poseNet(video, modelLoaded);
+     poseNet.on("pose",gotPosses);
+
 }
+
+ function modelLoaded() {
+     console.log('Model is Loaded');
+ }
+
+ function gotPosses(results) {
+     if (results.length>0) {
+         console.log(results);
+         left_Wrist_x = results[0].pose.leftWrist.x;
+         left_Wrist_y = results[0].pose.leftWrist.y;
+
+         right_Wrist_x = results[0].pose.rightWrist.x;
+         right_Wrist_y = results[0].pose.rightWrist.y;
+
+        console.log("Left wrist x is "+ left_Wrist_x + " and Right wrist x is "+right_Wrist_x);
+        console.log("Left wrist y is "+ left_Wrist_y + " and Right wrist y is "+right_Wrist_y);
+     }
+ }
 
  function draw() {
      image(video,0,0,600,600);
  }
+
+  function play() {
+      song.play();
+      song.setVolume(1);
+      song.rate(1);
+  }
+
+
+  function stop() {
+      song.stop();
+  }
