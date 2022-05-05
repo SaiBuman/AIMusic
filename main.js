a67702086dfe10b7b836c658e2ef6812ac014ec4
Sai Buman <78092854+SaiBuman@users.noreply.github.com>
2022-05-05 23:44:53 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,48 @@
+song1 = "";
+song2 = "";
+
+left_Wrist_x = "" ;
+right_Wrist_x = "" ;
+
+left_Wrist_y = "" ;
+right_Wrist_y = "" ;
+
+
+function preload() {
+    song1 = loadSound("Savage Love.mp3");
+    song2 = loadSound("Jugnu.mp3");
+
+}
+
+function setup() {
+    canvas = createCanvas(600,500);
+    canvas.center();
+
+    video = createCapture(VIDEO);
+    video.hide();
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on("pose",gotPosses);
+
+}
+
+function modelLoaded() {
+    console.log('Model is Loaded');
+}
+
+function gotPosses(results) {
+    if (results.length>0) {
+        console.log(results);
+        left_Wrist_x = results[0].pose.leftWrist.x;
+        left_Wrist_y = results[0].pose.leftWrist.y;
+
+        right_Wrist_x = results[0].pose.rightWrist.x;
+        right_Wrist_y = results[0].pose.rightWrist.y;
+
+       console.log("Left wrist x is "+ left_Wrist_x + " and Right wrist x is "+right_Wrist_x);
+       console.log("Left wrist y is "+ left_Wrist_y + " and Right wrist y is "+right_Wrist_y);
+    }
+}
+ function draw() {
+     image(video,0,0,600,600);
+ }
